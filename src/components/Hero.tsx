ac01ef8ef4861ad6fbb4582fae007d0e7d7cb818
nisarg3108 <bhavsarnisarg0@gmail.com>
2025-07-04 19:02:55 +0530

    Your message here

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,14 +53,6 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* Enhanced Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
-        <span className="text-white/60 text-sm mb-2 font-light tracking-wide">Discover More</span>
-        <div className="w-6 h-10 border-2 border-white/40 rounded-full flex justify-center">
-          <div className="w-1 h-3 bg-white/60 rounded-full mt-2 animate-pulse"></div>
-        </div>
-      </div>
-
       {/* Decorative Corner Elements */}
       <div className="absolute top-8 left-8 w-16 h-16 border-l-2 border-t-2 border-white/30 opacity-60"></div>
       <div className="absolute bottom-8 right-8 w-16 h-16 border-r-2 border-b-2 border-white/30 opacity-60"></div>
